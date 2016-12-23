ef6d1e0a12773cb8e772eed06b0b684dcce2b1be
Andrew Fernandes <afernan4e@gmail.com>
2016-12-23 11:10:59 -0500

    Add sha-512 hash and hmac support

--- okio/src/test/java/okio/HashingTest.java
+++ okio/src/test/java/okio/HashingTest.java
@@ -35,10 +35,18 @@ public final class HashingTest {
       "cb8379ac2098aa165029e3938a51da0bcecfc008fd6795f401178647f96c5b34");
   public static final ByteString SHA256_r32k = ByteString.decodeHex(
       "3a640aa4d129671cb36a4bfbed652d732bce6b7ea83ccdd080c485b8c9ef479d");
+  public static final ByteString SHA512_abc = ByteString.decodeHex(
+          "ddaf35a193617abacc417349ae20413112e6fa4e89a97ea20a9eeee64b55d39a2192992a274fc1a836ba3c23a3feebbd454d4423643ce80e2a9ac94fa54ca49f");
+  public static final ByteString SHA512_empty = ByteString.decodeHex(
+          "cf83e1357eefb8bdf1542850d66d8007d620e4050b5715dc83f4a921d36ce9ce47d0d13c5d85f2b0ff8318d2877eec2f63b931bd47417a81a538327af927da3e");
   public static final ByteString HMAC_SHA256_empty = ByteString.decodeHex(
       "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855");
   public static final ByteString HMAC_SHA256_abc = ByteString.decodeHex(
       "446d1715583cf1c30dfffbec0df4ff1f9d39d493211ab4c97ed6f3f0eb579b47");
+  public static final ByteString HMAC_SHA512_empty = ByteString.decodeHex(
+          "cf83e1357eefb8bdf1542850d66d8007d620e4050b5715dc83f4a921d36ce9ce47d0d13c5d85f2b0ff8318d2877eec2f63b931bd47417a81a538327af927da3e");
+  public static final ByteString HMAC_SHA512_abc = ByteString.decodeHex(
+          "24391790e7131050b05b606f2079a8983313894a1642a5ed97d094e7cabd00cfaa857d92c1f320ca3b6aaabb84c7155d6f1b10940dc133ded1b40baee8900be6");
   public static final ByteString r32k = TestUtil.randomBytes(32768);
 
   @Test public void byteStringMd5() {
@@ -53,12 +61,20 @@ public final class HashingTest {
     assertEquals(SHA256_abc, ByteString.encodeUtf8("abc").sha256());
   }
 
+  @Test public void byteStringSha512() {
+    assertEquals(SHA512_abc, ByteString.encodeUtf8("abc").sha512());
+  }
+
   @Test public void byteStringHmacSha1() {
     assertEquals(HMAC_SHA1_abc, ByteString.encodeUtf8("abc").hmacSha1(HMAC_KEY));
   }
 
   @Test public void byteStringHmacSha256() {
     assertEquals(HMAC_SHA256_abc, ByteString.encodeUtf8("abc").hmacSha256(HMAC_KEY));
+  }
+
+  @Test public void byteStringHmacSha512() {
+    assertEquals(HMAC_SHA512_abc, ByteString.encodeUtf8("abc").hmacSha512(HMAC_KEY));
   }
 
   @Test public void bufferMd5() {
@@ -73,8 +89,16 @@ public final class HashingTest {
     assertEquals(SHA256_abc, new Buffer().writeUtf8("abc").sha256());
   }
 
-  @Test public void hashEmptyBuffer() {
+  @Test public void bufferSha512() {
+    assertEquals(SHA512_abc, new Buffer().writeUtf8("abc").sha512());
+  }
+
+  @Test public void hashEmptySha256Buffer() {
     assertEquals(SHA256_empty, new Buffer().sha256());
+  }
+
+  @Test public void hashEmptySha512Buffer() {
+    assertEquals(SHA512_empty, new Buffer().sha512());
   }
 
   @Test public void bufferHmacSha1() {
@@ -85,8 +109,16 @@ public final class HashingTest {
     assertEquals(HMAC_SHA256_abc, new Buffer().writeUtf8("abc").hmacSha256(HMAC_KEY));
   }
 
-  @Test public void hmacEmptyBuffer() {
+  @Test public void bufferHmacSha512() {
+    assertEquals(HMAC_SHA512_abc, new Buffer().writeUtf8("abc").hmacSha512(HMAC_KEY));
+  }
+
+  @Test public void hmacSha256EmptyBuffer() {
     assertEquals(HMAC_SHA256_empty, new Buffer().sha256());
+  }
+
+  @Test public void hmacSha512EmptyBuffer() {
+    assertEquals(HMAC_SHA512_empty, new Buffer().sha512());
   }
 
   @Test public void bufferHashIsNotDestructive() {
