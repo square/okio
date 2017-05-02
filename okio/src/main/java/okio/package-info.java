2320c7f6ad9d05c1b65fb1e8cc43e257965792b4
jwilson <jwilson@squareup.com>
2017-05-01 22:39:52 -0400

    Import jsr305 and use it to mark @Nullable stuff.
    The result is that IntelliJ + Kotlin correctly infers the right nullability
    for Okio's APIs. I used IntelliJ's "Specify types explicitly" quick fix on
    the 'a' and 'b' variables below and it did the right thing.
    
        fun specifyTypeExplicitly(source: BufferedSource) {
          val a: String? = source.readUtf8Line()
          val b: String = source.readUtf8LineStrict()
        }

--- okio/src/main/java/okio/package-info.java
+++ okio/src/main/java/okio/package-info.java
@@ -2,4 +2,5 @@
  * Okio complements {@link java.io} and {@link java.nio} to make it much easier to access, store,
  * and process your data.
  */
+@javax.annotation.ParametersAreNonnullByDefault
 package okio;
