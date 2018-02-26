56a6a69e956a7ad4003328c3c4311321e649367a
John <joberhauser@solstice.com>
2018-02-26 16:39:32 -0600

    catching iae

--- okio/src/main/java/okio/Okio.java
+++ okio/src/main/java/okio/Okio.java
@@ -116,7 +116,11 @@ public final class Okio {
     if (socket == null) throw new IllegalArgumentException("socket == null");
     if (socket.getOutputStream() == null) throw new IOException("socket's output stream == null");
     AsyncTimeout timeout = timeout(socket);
-    Sink sink = sink(socket.getOutputStream(), timeout);
+    try {
+      Sink sink = sink(socket.getOutputStream(), timeout);
+    } catch (IllegalArgumentException e) {
+      throw new IOException("something was null");
+    }
     return timeout.sink(sink);
   }
 
@@ -222,7 +226,11 @@ public final class Okio {
     if (socket == null) throw new IllegalArgumentException("socket == null");
     if (socket.getInputStream() == null) throw new IOException("socket's input stream == null");
     AsyncTimeout timeout = timeout(socket);
-    Source source = source(socket.getInputStream(), timeout);
+    try {
+      Source source = source(socket.getInputStream(), timeout);
+    } catch (IllegalArgumentException e) {
+      throw new IOException("something was null");
+    }
     return timeout.source(source);
   }
 
