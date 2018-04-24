d04d30dd1d02e32226098257d8cefd0e16b07aa2
Jake Wharton <jakew@google.com>
2018-04-24 11:17:17 -0400

    Port HashingSink/Source to Kotlin

---
+++ okio/src/main/java/okio/HashingSink.kt
@@ -0,0 +1,140 @@
+/*
+ * Copyright (C) 2016 Square, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package okio
+
+import java.io.IOException
+import java.security.InvalidKeyException
+import java.security.MessageDigest
+import java.security.NoSuchAlgorithmException
+import javax.crypto.Mac
+import javax.crypto.spec.SecretKeySpec
+
+import okio.Util.checkOffsetAndCount
+
+/**
+ * A sink that computes a hash of the full stream of bytes it has accepted. To use, create an
+ * instance with your preferred hash algorithm. Write all of the data to the sink and then call
+ * [hash] to compute the final hash value.
+ *
+ * In this example we use `HashingSink` with a [BufferedSink] to make writing to the
+ * sink easier.
+ * ```
+ * HashingSink hashingSink = HashingSink.sha256(s);
+ * BufferedSink bufferedSink = Okio.buffer(hashingSink);
+ *
+ * ... // Write to bufferedSink and either flush or close it.
+ *
+ * ByteString hash = hashingSink.hash();
+ * ```
+ */
+class HashingSink : ForwardingSink {
+  private val messageDigest: MessageDigest?
+  private val mac: Mac?
+
+  internal constructor(sink: Sink, algorithm: String) : super(sink) {
+    try {
+      this.messageDigest = MessageDigest.getInstance(algorithm)
+      this.mac = null
+    } catch (e: NoSuchAlgorithmException) {
+      throw AssertionError()
+    }
+  }
+
+  internal constructor(sink: Sink, key: ByteString, algorithm: String) : super(sink) {
+    try {
+      this.mac = Mac.getInstance(algorithm).apply {
+        init(SecretKeySpec(key.toByteArray(), algorithm))
+      }
+      this.messageDigest = null
+    } catch (e: NoSuchAlgorithmException) {
+      throw AssertionError()
+    } catch (e: InvalidKeyException) {
+      throw IllegalArgumentException(e)
+    }
+  }
+
+  @Throws(IOException::class)
+  override fun write(source: Buffer, byteCount: Long) {
+    checkOffsetAndCount(source.size, 0, byteCount)
+
+    // Hash byteCount bytes from the prefix of source.
+    var hashedCount = 0L
+    var s = source.head!!
+    while (hashedCount < byteCount) {
+      val toHash = minOf(byteCount - hashedCount, (s.limit - s.pos).toLong()).toInt()
+      if (messageDigest != null) {
+        messageDigest.update(s.data, s.pos, toHash)
+      } else {
+        mac!!.update(s.data, s.pos, toHash)
+      }
+      hashedCount += toHash
+      s = s.next
+    }
+
+    // Write those bytes to the sink.
+    super.write(source, byteCount)
+  }
+
+  /**
+   * Returns the hash of the bytes accepted thus far and resets the internal state of this sink.
+   *
+   * **Warning:** This method is not idempotent. Each time this method is called its
+   * internal state is cleared. This starts a new hash with zero bytes accepted.
+   */
+  @Suppress("NON_FINAL_MEMBER_IN_FINAL_CLASS") // Required to keep JApicmp happy.
+  @get:JvmName("hash")
+  open val hash: ByteString get() {
+    val result = if (messageDigest != null) messageDigest.digest() else mac!!.doFinal()
+    return ByteString.of(*result)
+  }
+
+  companion object {
+    /** Returns a sink that uses the obsolete MD5 hash algorithm to produce 128-bit hashes. */
+    @JvmStatic fun md5(sink: Sink): HashingSink {
+      return HashingSink(sink, "MD5")
+    }
+
+    /** Returns a sink that uses the obsolete SHA-1 hash algorithm to produce 160-bit hashes. */
+    @JvmStatic fun sha1(sink: Sink): HashingSink {
+      return HashingSink(sink, "SHA-1")
+    }
+
+    /** Returns a sink that uses the SHA-256 hash algorithm to produce 256-bit hashes. */
+    @JvmStatic fun sha256(sink: Sink): HashingSink {
+      return HashingSink(sink, "SHA-256")
+    }
+
+    /** Returns a sink that uses the SHA-512 hash algorithm to produce 512-bit hashes. */
+    @JvmStatic fun sha512(sink: Sink): HashingSink {
+      return HashingSink(sink, "SHA-512")
+    }
+
+    /** Returns a sink that uses the obsolete SHA-1 HMAC algorithm to produce 160-bit hashes. */
+    @JvmStatic fun hmacSha1(sink: Sink, key: ByteString): HashingSink {
+      return HashingSink(sink, key, "HmacSHA1")
+    }
+
+    /** Returns a sink that uses the SHA-256 HMAC algorithm to produce 256-bit hashes. */
+    @JvmStatic fun hmacSha256(sink: Sink, key: ByteString): HashingSink {
+      return HashingSink(sink, key, "HmacSHA256")
+    }
+
+    /** Returns a sink that uses the SHA-512 HMAC algorithm to produce 512-bit hashes. */
+    @JvmStatic fun hmacSha512(sink: Sink, key: ByteString): HashingSink {
+      return HashingSink(sink, key, "HmacSHA512")
+    }
+  }
+}
---
+++ okio/src/main/java/okio/HashingSource.kt
@@ -0,0 +1,149 @@
+/*
+ * Copyright (C) 2016 Square, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package okio
+
+import java.io.IOException
+import java.security.InvalidKeyException
+import java.security.MessageDigest
+import java.security.NoSuchAlgorithmException
+import javax.crypto.Mac
+import javax.crypto.spec.SecretKeySpec
+
+/**
+ * A source that computes a hash of the full stream of bytes it has supplied. To use, create an
+ * instance with your preferred hash algorithm. Exhaust the source by reading all of its bytes and
+ * then call [hash] to compute the final hash value.
+ *
+ *
+ * In this example we use `HashingSource` with a [BufferedSource] to make reading
+ * from the source easier.
+ * ```
+ * HashingSource hashingSource = HashingSource.sha256(rawSource);
+ * BufferedSource bufferedSource = Okio.buffer(hashingSource);
+ *
+ * ... // Read all of bufferedSource.
+ *
+ * ByteString hash = hashingSource.hash();
+ * ```
+ */
+class HashingSource : ForwardingSource {
+  private val messageDigest: MessageDigest?
+  private val mac: Mac?
+
+  internal constructor(source: Source, algorithm: String) : super(source) {
+    try {
+      messageDigest = MessageDigest.getInstance(algorithm)
+      mac = null
+    } catch (e: NoSuchAlgorithmException) {
+      throw AssertionError()
+    }
+  }
+
+  internal constructor(source: Source, key: ByteString, algorithm: String) : super(source) {
+    try {
+      mac = Mac.getInstance(algorithm).apply {
+        init(SecretKeySpec(key.toByteArray(), algorithm))
+      }
+      messageDigest = null
+    } catch (e: NoSuchAlgorithmException) {
+      throw AssertionError()
+    } catch (e: InvalidKeyException) {
+      throw IllegalArgumentException(e)
+    }
+  }
+
+  @Throws(IOException::class)
+  override fun read(sink: Buffer, byteCount: Long): Long {
+    val result = super.read(sink, byteCount)
+
+    if (result != -1L) {
+      var start = sink.size - result
+
+      // Find the first segment that has new bytes.
+      var offset = sink.size
+      var s = sink.head!!
+      while (offset > start) {
+        s = s.prev
+        offset -= (s.limit - s.pos).toLong()
+      }
+
+      // Hash that segment and all the rest until the end.
+      while (offset < sink.size) {
+        val pos = (s.pos + start - offset).toInt()
+        if (messageDigest != null) {
+          messageDigest.update(s.data, pos, s.limit - pos)
+        } else {
+          mac!!.update(s.data, pos, s.limit - pos)
+        }
+        offset += s.limit - s.pos
+        start = offset
+        s = s.next
+      }
+    }
+
+    return result
+  }
+
+  /**
+   * Returns the hash of the bytes supplied thus far and resets the internal state of this source.
+   *
+   * **Warning:** This method is not idempotent. Each time this method is called its
+   * internal state is cleared. This starts a new hash with zero bytes supplied.
+   */
+  @Suppress("NON_FINAL_MEMBER_IN_FINAL_CLASS") // Required to keep JApicmp happy.
+  @get:JvmName("hash")
+  open val hash: ByteString get() {
+    val result = if (messageDigest != null) messageDigest.digest() else mac!!.doFinal()
+    return ByteString.of(*result)
+  }
+
+  companion object {
+    /** Returns a sink that uses the obsolete MD5 hash algorithm to produce 128-bit hashes. */
+    @JvmStatic fun md5(source: Source): HashingSource {
+      return HashingSource(source, "MD5")
+    }
+
+    /** Returns a sink that uses the obsolete SHA-1 hash algorithm to produce 160-bit hashes. */
+    @JvmStatic fun sha1(source: Source): HashingSource {
+      return HashingSource(source, "SHA-1")
+    }
+
+    /** Returns a sink that uses the SHA-256 hash algorithm to produce 256-bit hashes. */
+    @JvmStatic fun sha256(source: Source): HashingSource {
+      return HashingSource(source, "SHA-256")
+    }
+
+    /** Returns a sink that uses the SHA-512 hash algorithm to produce 512-bit hashes. */
+    @JvmStatic fun sha512(source: Source): HashingSource {
+      return HashingSource(source, "SHA-512")
+    }
+
+    /** Returns a sink that uses the obsolete SHA-1 HMAC algorithm to produce 160-bit hashes. */
+    @JvmStatic fun hmacSha1(source: Source, key: ByteString): HashingSource {
+      return HashingSource(source, key, "HmacSHA1")
+    }
+
+    /** Returns a sink that uses the SHA-256 HMAC algorithm to produce 256-bit hashes. */
+    @JvmStatic fun hmacSha256(source: Source, key: ByteString): HashingSource {
+      return HashingSource(source, key, "HmacSHA256")
+    }
+
+    /** Returns a sink that uses the SHA-512 HMAC algorithm to produce 512-bit hashes. */
+    @JvmStatic fun hmacSha512(source: Source, key: ByteString): HashingSource {
+      return HashingSource(source, key, "HmacSHA512")
+    }
+  }
+}
