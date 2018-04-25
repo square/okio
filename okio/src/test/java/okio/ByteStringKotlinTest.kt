42b1af1c5b1d743981a97979781ae0ea0816c5c1
Jesse Wilson <jwilson@squareup.com>
2018-04-25 11:10:46 -0400

    Convert Base64 to Kotlin

--- okio/src/test/java/okio/ByteStringKotlinTest.kt
+++ okio/src/test/java/okio/ByteStringKotlinTest.kt
@@ -13,24 +13,28 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package okio
 
+import okio.ByteString.Companion.decodeBase64
 import okio.ByteString.Companion.decodeHex
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
-import kotlin.test.assertEquals
 
-class GzipKotlinTest {
-  @Test fun sink() {
-    val data = Buffer()
-    val gzip = (data as Sink).gzip()
-    gzip.buffer().writeUtf8("Hi!").close()
-    assertEquals("1f8b0800000000000000f3c8540400dac59e7903000000", data.readByteString().hex())
+class ByteStringKotlinTest {
+  @Test fun decodeBase64() {
+    val actual = "YfCfjalj".decodeBase64()
+    val expected = ByteString.encodeUtf8("a\uD83C\uDF69c")
+    assertThat(actual).isEqualTo(expected)
   }
 
-  @Test fun source() {
-    val buffer = Buffer().write("1f8b0800000000000000f3c8540400dac59e7903000000".decodeHex())
-    val gzip = (buffer as Source).gzip()
-    assertEquals("Hi!", gzip.buffer().readUtf8())
+  @Test fun decodeBase64Invalid() {
+    val actual = ";-)".decodeBase64()
+    assertThat(actual).isNull()
+  }
+
+  @Test fun decodeHex() {
+    val actual = "CAFEBABE".decodeHex()
+    val expected = ByteString.of(-54, -2, -70, -66)
+    assertThat(actual).isEqualTo(expected)
   }
 }
