6e823d812b6f2c98ca142615aa0a63520b5a2ec5
Egor Andreevici <egor@squareup.com>
2019-08-22 18:43:11 -0400

    Remove unused import

--- okio/src/jvmTest/kotlin/okio/BufferCursorKotlinTest.kt
+++ okio/src/jvmTest/kotlin/okio/BufferCursorKotlinTest.kt
@@ -23,7 +23,6 @@ import org.junit.runner.RunWith
 import org.junit.runners.Parameterized
 import org.junit.runners.Parameterized.Parameter
 import org.junit.runners.Parameterized.Parameters
-import java.util.Arrays
 import kotlin.test.assertEquals
 import kotlin.test.assertFalse
 import kotlin.test.assertNotSame
