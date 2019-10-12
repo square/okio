d1eaf3d147cff4cc842e1defdf0e366566dba9b1
Egor Andreevich <andreevich.egor@gmail.com>
2019-10-12 08:44:27 -0400

    Remove unused import

--- okio/src/jvmTest/kotlin/okio/TestUtil.kt
+++ okio/src/jvmTest/kotlin/okio/TestUtil.kt
@@ -20,7 +20,6 @@ import java.io.IOException
 import java.io.ObjectInputStream
 import java.io.ObjectOutputStream
 import java.io.Serializable
-import java.util.Arrays
 import java.util.Random
 import kotlin.test.assertEquals
 import kotlin.test.assertFalse
