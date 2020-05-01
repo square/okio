def4fc06f7666b978859147d0a10e1c1c155b99f
Martin Devillers <martin.devillers@dashlane.com>
2020-05-01 18:53:52 +0200

    Cipher tests with single byte updates
    Failing for cipher source

--- okio/src/jvmTest/kotlin/okio/CipherSinkTest.kt
+++ okio/src/jvmTest/kotlin/okio/CipherSinkTest.kt
@@ -6,7 +6,7 @@ import kotlin.random.Random
 class CipherSinkTest {
 
   @Test
-  fun encryptWithClose() {
+  fun encrypt() {
     val random = Random(1588326457426L)
     val data = random.nextBytes(32)
     val key = random.nextBytes(16)
@@ -21,7 +21,22 @@ class CipherSinkTest {
   }
 
   @Test
-  fun decryptWithClose() {
+  fun encryptSingleByteWrite() {
+    val random = Random(1588326457426L)
+    val data = random.nextBytes(32)
+    val key = random.nextBytes(16)
+    val cipherFactory = CipherFactory(key)
+
+    val buffer = Buffer()
+    buffer.cipherSink(cipherFactory.encrypt).buffer().use { data.forEach { byte -> it.writeByte(byte.toInt()) }}
+    val actualEncryptedData = buffer.readByteArray()
+
+    val expectedEncryptedData = cipherFactory.encrypt.doFinal(data)
+    assertArrayEquals(expectedEncryptedData, actualEncryptedData)
+  }
+
+  @Test
+  fun decrypt() {
     val random = Random(1588326610176L)
     val key = random.nextBytes(16)
     val cipherFactory = CipherFactory(key)
@@ -30,6 +45,21 @@ class CipherSinkTest {
 
     val buffer = Buffer()
     buffer.cipherSink(cipherFactory.decrypt).buffer().use { it.write(encryptedData) }
+    val actualData = buffer.readByteArray()
+
+    assertArrayEquals(expectedData, actualData)
+  }
+
+  @Test
+  fun decryptSingleByteWrite() {
+    val random = Random(1588326610176L)
+    val key = random.nextBytes(16)
+    val cipherFactory = CipherFactory(key)
+    val expectedData = random.nextBytes(32)
+    val encryptedData = cipherFactory.encrypt.doFinal(expectedData)
+
+    val buffer = Buffer()
+    buffer.cipherSink(cipherFactory.decrypt).buffer().use { encryptedData.forEach { byte -> it.writeByte(byte.toInt()) } }
     val actualData = buffer.readByteArray()
 
     assertArrayEquals(expectedData, actualData)
--- okio/src/jvmTest/kotlin/okio/CipherSourceTest.kt
+++ okio/src/jvmTest/kotlin/okio/CipherSourceTest.kt
@@ -6,21 +6,37 @@ import kotlin.random.Random
 class CipherSourceTest {
 
   @Test
-  fun encryptWithClose() {
+  fun encrypt() {
     val random = Random(1588326457426L)
     val data = random.nextBytes(32)
     val key = random.nextBytes(16)
     val cipherFactory = CipherFactory(key)
 
     val buffer = Buffer().apply { write(data) }
-    val actualEncryptedData = buffer.cipherSource(cipherFactory.encrypt).buffer().use { it.readByteArray() }
+    val actualEncryptedData =
+      buffer.cipherSource(cipherFactory.encrypt).buffer().use { it.readByteArray() }
 
     val expectedEncryptedData = cipherFactory.encrypt.doFinal(data)
     assertArrayEquals(expectedEncryptedData, actualEncryptedData)
   }
 
   @Test
-  fun decryptWithClose() {
+  fun encryptSingleByteSource() {
+    val random = Random(1588326457426L)
+    val data = random.nextBytes(32)
+    val key = random.nextBytes(16)
+    val cipherFactory = CipherFactory(key)
+
+    val buffer = Buffer().apply { write(data) }
+    val actualEncryptedData =
+      buffer.emitSingleBytes().cipherSource(cipherFactory.encrypt).buffer().use { it.readByteArray() }
+
+    val expectedEncryptedData = cipherFactory.encrypt.doFinal(data)
+    assertArrayEquals(expectedEncryptedData, actualEncryptedData)
+  }
+
+  @Test
+  fun decrypt() {
     val random = Random(1588326610176L)
     val key = random.nextBytes(16)
     val cipherFactory = CipherFactory(key)
@@ -28,8 +44,44 @@ class CipherSourceTest {
     val encryptedData = cipherFactory.encrypt.doFinal(expectedData)
 
     val buffer = Buffer().apply { write(encryptedData) }
-    val actualData = buffer.cipherSource(cipherFactory.decrypt).buffer().use { it.readByteArray() }
+    val actualData =
+      buffer.cipherSource(cipherFactory.decrypt).buffer().use { it.readByteArray() }
 
     assertArrayEquals(expectedData, actualData)
+  }
+
+  @Test
+  fun decryptSingleByteSource() {
+    val random = Random(1588326610176L)
+    val key = random.nextBytes(16)
+    val cipherFactory = CipherFactory(key)
+    val expectedData = random.nextBytes(32)
+    val encryptedData = cipherFactory.encrypt.doFinal(expectedData)
+
+    val buffer = Buffer().apply { write(encryptedData) }
+    val actualData =
+      buffer.emitSingleBytes().cipherSource(cipherFactory.decrypt).buffer().use { it.readByteArray() }
+
+    assertArrayEquals(expectedData, actualData)
+  }
+}
+
+private fun BufferedSource.emitSingleBytes(): Source =
+  SingleByteSource(this)
+
+private class SingleByteSource(private val source: BufferedSource) : Source {
+  override fun read(sink: Buffer, byteCount: Long): Long =
+    if (source.exhausted()) {
+      -1
+    } else {
+      sink.writeByte(source.readByte().toInt())
+      1
+    }
+
+  override fun timeout(): Timeout =
+    source.timeout()
+
+  override fun close() {
+    source.close()
   }
 }
