35a459b86620736b07c6a188bb6d9eff9ad6c835
Martin Devillers <martin.devillers@dashlane.com>
2020-05-02 19:17:24 +0200

    Standard file structure
    License, JVM file name, inline extension.

--- okio/src/jvmMain/kotlin/okio/CipherSource.kt
+++ okio/src/jvmMain/kotlin/okio/CipherSource.kt
@@ -1,3 +1,22 @@
+/*
+ * Copyright (C) 2014 Square, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+@file:JvmName("-CipherSourceExtensions")
+@file:Suppress("NOTHING_TO_INLINE") // Aliases to public API.
+
 package okio
 
 import java.io.IOException
@@ -90,5 +109,5 @@ class CipherSource internal constructor(private val source: BufferedSource, priv
   }
 }
 
-fun Source.cipherSource(cipher: Cipher): CipherSource =
+inline fun Source.cipherSource(cipher: Cipher): CipherSource =
   CipherSource(this, cipher)
