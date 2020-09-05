9d2cef74211d94d0e0ca8c0d283a6fdc504326d2
Kevin Cianfarini <kevincianfarini@gmail.com>
2020-09-05 16:54:55 -0400

    Add OkioMessageDigest JMH benchmarks

--- okio/src/commonMain/kotlin/okio/internal/OkioMessageDigest.kt
+++ okio/src/commonMain/kotlin/okio/internal/OkioMessageDigest.kt
@@ -1,13 +1,13 @@
 package okio.internal
 
-internal interface OkioMessageDigest {
+interface OkioMessageDigest {
 
   fun update(input: ByteArray)
 
   fun digest(): ByteArray
 }
 
-internal fun newMessageDigest(algorithm: String): OkioMessageDigest = when (algorithm) {
+fun newMessageDigest(algorithm: String): OkioMessageDigest = when (algorithm) {
   "SHA-1" -> Sha1MessageDigest()
   "SHA-256" -> Sha256MessageDigest()
   "SHA-512" -> Sha512MessageDigest()
