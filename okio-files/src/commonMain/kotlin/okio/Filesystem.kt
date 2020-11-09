26596b5567f58c656998219d6e1a4e2b1a9b6152
Martin Bonnin <martin@mbonnin.net>
2020-11-09 11:53:13 +0100

    remove bad javadoc

--- okio-files/src/commonMain/kotlin/okio/Filesystem.kt
+++ okio-files/src/commonMain/kotlin/okio/Filesystem.kt
@@ -113,11 +113,6 @@ abstract class Filesystem {
 
   /**
    * Creates a temporary directory
-   *
-   * @throws IOException if there is nothing at [path] to delete, or if there is a file or directory
-   *     but it could not be deleted. Deletes fail if the current process doesn't have access, if
-   *     the filesystem is readonly, or if [path] is a non-empty directory.  This list of potential
-   *     problems is not exhaustive.
    */
   abstract fun tmpDirectory(): String
 
