dda164c3575952df497385aeba39d814910ab535
pauldavies83 <pauldavies83@googlemail.com>
2021-10-18 20:40:00 +0100

    add apple silicon (m1) simulator targets

--- build-support/src/main/kotlin/platforms.kt
+++ build-support/src/main/kotlin/platforms.kt
@@ -6,11 +6,14 @@ import org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet
 fun KotlinMultiplatformExtension.configureOrCreateNativePlatforms() {
   iosX64()
   iosArm64()
+  iosSimulatorArm64()
   tvosX64()
   tvosArm64()
+  tvosSimulatorArm64()
   watchosArm32()
   watchosArm64()
   watchosX86()
+  watchosSimulatorArm64()
   // Required to generate tests tasks: https://youtrack.jetbrains.com/issue/KT-26547
   linuxX64()
   macosX64()
@@ -21,13 +24,16 @@ fun KotlinMultiplatformExtension.configureOrCreateNativePlatforms() {
 val appleTargets = listOf(
   "iosArm64",
   "iosX64",
+  "iosSimulatorArm64",
   "macosX64",
   "macosArm64",
   "tvosArm64",
   "tvosX64",
+  "tvosSimulatorArm64",
   "watchosArm32",
   "watchosArm64",
-  "watchosX86"
+  "watchosX86",
+  "watchosSimulatorArm64"
 )
 
 val mingwTargets = listOf(
@@ -50,11 +56,14 @@ val unixSizet32Targets = listOf(
 val unixSizet64Targets = listOf(
   "iosArm64",
   "iosX64",
+  "iosSimulatorArm64",
   "linuxX64",
   "macosX64",
   "macosArm64",
   "tvosArm64",
-  "tvosX64"
+  "tvosX64",
+  "tvosSimulatorArm64",
+  "watchosSimulatorArm64"
 )
 
 /**
