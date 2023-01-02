22516e5324e1c0bdecb67cc648506c60b17c7d92
Yuri Schimke <yuri@schimke.ee>
2023-01-02 17:26:35 +1000

    Fix formatting.

--- okio/src/jvmTest/java/okio/AwaitSignalTest.kt
+++ okio/src/jvmTest/java/okio/AwaitSignalTest.kt
@@ -26,147 +26,147 @@ import java.util.concurrent.locks.Condition
 import java.util.concurrent.locks.ReentrantLock
 
 class AwaitSignalTest {
-    val executorService = Executors.newScheduledThreadPool(0)
+  val executorService = Executors.newScheduledThreadPool(0)
 
-    val lock: ReentrantLock = ReentrantLock()
-    val condition: Condition = lock.newCondition()
+  val lock: ReentrantLock = ReentrantLock()
+  val condition: Condition = lock.newCondition()
 
-    @After
-    fun tearDown() {
-        executorService.shutdown()
+  @After
+  fun tearDown() {
+    executorService.shutdown()
+  }
+
+  @Test
+  fun notified() = lock.withLock {
+    val timeout = Timeout()
+    timeout.timeout(5000, TimeUnit.MILLISECONDS)
+    val start = now()
+    executorService.schedule(
+      { lock.withLock { condition.signal() } },
+      1000,
+      TimeUnit.MILLISECONDS
+    )
+    timeout.awaitSignal(condition)
+    assertElapsed(1000.0, start)
+  }
+
+  @Test
+  fun timeout() = lock.withLock {
+    assumeNotWindows()
+    val timeout = Timeout()
+    timeout.timeout(1000, TimeUnit.MILLISECONDS)
+    val start = now()
+    try {
+      timeout.awaitSignal(condition)
+      Assert.fail()
+    } catch (expected: InterruptedIOException) {
+      Assert.assertEquals("timeout", expected.message)
     }
+    assertElapsed(1000.0, start)
+  }
 
-    @Test
-    fun notified() = lock.withLock {
-        val timeout = Timeout()
-        timeout.timeout(5000, TimeUnit.MILLISECONDS)
-        val start = now()
-        executorService.schedule(
-            { lock.withLock { condition.signal() } },
-            1000,
-            TimeUnit.MILLISECONDS
-        )
-        timeout.awaitSignal(condition)
-        assertElapsed(1000.0, start)
+  @Test
+  fun deadline() = lock.withLock {
+    assumeNotWindows()
+    val timeout = Timeout()
+    timeout.deadline(1000, TimeUnit.MILLISECONDS)
+    val start = now()
+    try {
+      timeout.awaitSignal(condition)
+      Assert.fail()
+    } catch (expected: InterruptedIOException) {
+      Assert.assertEquals("timeout", expected.message)
     }
+    assertElapsed(1000.0, start)
+  }
 
-    @Test
-    fun timeout() = lock.withLock {
-        assumeNotWindows()
-        val timeout = Timeout()
-        timeout.timeout(1000, TimeUnit.MILLISECONDS)
-        val start = now()
-        try {
-            timeout.awaitSignal(condition)
-            Assert.fail()
-        } catch (expected: InterruptedIOException) {
-            Assert.assertEquals("timeout", expected.message)
-        }
-        assertElapsed(1000.0, start)
+  @Test
+  fun deadlineBeforeTimeout() = lock.withLock {
+    assumeNotWindows()
+    val timeout = Timeout()
+    timeout.timeout(5000, TimeUnit.MILLISECONDS)
+    timeout.deadline(1000, TimeUnit.MILLISECONDS)
+    val start = now()
+    try {
+      timeout.awaitSignal(condition)
+      Assert.fail()
+    } catch (expected: InterruptedIOException) {
+      Assert.assertEquals("timeout", expected.message)
     }
+    assertElapsed(1000.0, start)
+  }
 
-    @Test
-    fun deadline() = lock.withLock {
-        assumeNotWindows()
-        val timeout = Timeout()
-        timeout.deadline(1000, TimeUnit.MILLISECONDS)
-        val start = now()
-        try {
-            timeout.awaitSignal(condition)
-            Assert.fail()
-        } catch (expected: InterruptedIOException) {
-            Assert.assertEquals("timeout", expected.message)
-        }
-        assertElapsed(1000.0, start)
+  @Test
+  fun timeoutBeforeDeadline() = lock.withLock {
+    assumeNotWindows()
+    val timeout = Timeout()
+    timeout.timeout(1000, TimeUnit.MILLISECONDS)
+    timeout.deadline(5000, TimeUnit.MILLISECONDS)
+    val start = now()
+    try {
+      timeout.awaitSignal(condition)
+      Assert.fail()
+    } catch (expected: InterruptedIOException) {
+      Assert.assertEquals("timeout", expected.message)
     }
+    assertElapsed(1000.0, start)
+  }
 
-    @Test
-    fun deadlineBeforeTimeout() = lock.withLock {
-        assumeNotWindows()
-        val timeout = Timeout()
-        timeout.timeout(5000, TimeUnit.MILLISECONDS)
-        timeout.deadline(1000, TimeUnit.MILLISECONDS)
-        val start = now()
-        try {
-            timeout.awaitSignal(condition)
-            Assert.fail()
-        } catch (expected: InterruptedIOException) {
-            Assert.assertEquals("timeout", expected.message)
-        }
-        assertElapsed(1000.0, start)
+  @Test
+  fun deadlineAlreadyReached() = lock.withLock {
+    assumeNotWindows()
+    val timeout = Timeout()
+    timeout.deadlineNanoTime(System.nanoTime())
+    val start = now()
+    try {
+      timeout.awaitSignal(condition)
+      Assert.fail()
+    } catch (expected: InterruptedIOException) {
+      Assert.assertEquals("timeout", expected.message)
     }
+    assertElapsed(0.0, start)
+  }
 
-    @Test
-    fun timeoutBeforeDeadline() = lock.withLock {
-        assumeNotWindows()
-        val timeout = Timeout()
-        timeout.timeout(1000, TimeUnit.MILLISECONDS)
-        timeout.deadline(5000, TimeUnit.MILLISECONDS)
-        val start = now()
-        try {
-            timeout.awaitSignal(condition)
-            Assert.fail()
-        } catch (expected: InterruptedIOException) {
-            Assert.assertEquals("timeout", expected.message)
-        }
-        assertElapsed(1000.0, start)
+  @Test
+  fun threadInterrupted() = lock.withLock {
+    assumeNotWindows()
+    val timeout = Timeout()
+    val start = now()
+    Thread.currentThread().interrupt()
+    try {
+      timeout.awaitSignal(condition)
+      Assert.fail()
+    } catch (expected: InterruptedIOException) {
+      Assert.assertEquals("interrupted", expected.message)
+      Assert.assertTrue(Thread.interrupted())
     }
+    assertElapsed(0.0, start)
+  }
 
-    @Test
-    fun deadlineAlreadyReached() = lock.withLock {
-        assumeNotWindows()
-        val timeout = Timeout()
-        timeout.deadlineNanoTime(System.nanoTime())
-        val start = now()
-        try {
-            timeout.awaitSignal(condition)
-            Assert.fail()
-        } catch (expected: InterruptedIOException) {
-            Assert.assertEquals("timeout", expected.message)
-        }
-        assertElapsed(0.0, start)
+  @Test
+  fun threadInterruptedOnThrowIfReached() = lock.withLock {
+    assumeNotWindows()
+    val timeout = Timeout()
+    Thread.currentThread().interrupt()
+    try {
+      timeout.throwIfReached()
+      Assert.fail()
+    } catch (expected: InterruptedIOException) {
+      Assert.assertEquals("interrupted", expected.message)
+      Assert.assertTrue(Thread.interrupted())
     }
+  }
 
-    @Test
-    fun threadInterrupted() = lock.withLock {
-        assumeNotWindows()
-        val timeout = Timeout()
-        val start = now()
-        Thread.currentThread().interrupt()
-        try {
-            timeout.awaitSignal(condition)
-            Assert.fail()
-        } catch (expected: InterruptedIOException) {
-            Assert.assertEquals("interrupted", expected.message)
-            Assert.assertTrue(Thread.interrupted())
-        }
-        assertElapsed(0.0, start)
-    }
+  /** Returns the nanotime in milliseconds as a double for measuring timeouts.  */
+  private fun now(): Double {
+    return System.nanoTime() / 1000000.0
+  }
 
-    @Test
-    fun threadInterruptedOnThrowIfReached() = lock.withLock {
-        assumeNotWindows()
-        val timeout = Timeout()
-        Thread.currentThread().interrupt()
-        try {
-            timeout.throwIfReached()
-            Assert.fail()
-        } catch (expected: InterruptedIOException) {
-            Assert.assertEquals("interrupted", expected.message)
-            Assert.assertTrue(Thread.interrupted())
-        }
-    }
-
-    /** Returns the nanotime in milliseconds as a double for measuring timeouts.  */
-    private fun now(): Double {
-        return System.nanoTime() / 1000000.0
-    }
-
-    /**
-     * Fails the test unless the time from start until now is duration, accepting differences in
-     * -50..+450 milliseconds.
-     */
-    private fun assertElapsed(duration: Double, start: Double) {
-        Assert.assertEquals(duration, now() - start - 200.0, 250.0)
-    }
+  /**
+   * Fails the test unless the time from start until now is duration, accepting differences in
+   * -50..+450 milliseconds.
+   */
+  private fun assertElapsed(duration: Double, start: Double) {
+    Assert.assertEquals(duration, now() - start - 200.0, 250.0)
+  }
 }
