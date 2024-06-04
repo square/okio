7913508168561a02f05203717c131c7ac687350a
Clayton Walker <clayton.m.walker@gmail.com>
2024-06-03 23:55:16 -0600

    Update modular tests to be more reasonable

--- okio/src/jvmIntegrationTest/kotlin/okio/test/integration/ModuleTest.kt
+++ okio/src/jvmIntegrationTest/kotlin/okio/test/integration/ModuleTest.kt
@@ -1,7 +1,5 @@
 package okio.test.integration
 
-import okio.FileSystem
-import org.junit.Assert.assertEquals
 import org.junit.Assert.assertFalse
 import org.junit.Assert.assertTrue
 import org.junit.Test
@@ -9,13 +7,10 @@ import org.junit.Test
 class ModuleTest {
   @Test
   fun testModule() {
-    // test okio.test.integration is modular
-    assertTrue(ModuleTest::class.java.module.isNamed)
-    assertEquals(ModuleTest::class.java.module.name, "okio.test.integration")
-    assertFalse(ModuleTest::class.java.module.descriptor.isAutomatic)
-    // test okio is modular
-    assertTrue(FileSystem::class.java.module.isNamed)
-    assertEquals(FileSystem::class.java.module.name, "okio")
-    assertFalse(FileSystem::class.java.module.descriptor.isAutomatic)
+    // assert okio is modular
+    val okioModule = ModuleLayer.boot().modules().single { it.name == "okio" }
+    assertFalse(okioModule.descriptor.isAutomatic)
+    assertTrue(okioModule.isExported("okio"))
+    assertFalse(okioModule.isExported("okio.internal"))
   }
 }
