75190d69fd66b6e0af84e28b0ef88fd1617b8482
Jesse Wilson <jwilson@squareup.com>
2025-06-04 10:10:31 -0400

    New Channel interface
    I haven't yet hooked this up to Socket, but I intend to
    in a follow-up PR.

---
+++ okio/src/commonMain/kotlin/okio/Channel.kt
@@ -0,0 +1,44 @@
+/*
+ * Copyright (C) 2025 Square, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package okio
+
+/**
+ * A pair of streams for interactive communication with another machine.
+ *
+ * Send data to the peer by writing to [sink], and read data from the peer by reading from [source].
+ *
+ * This can be implemented by a plain TCP socket. It can also be layered to add features like
+ * security (as in a TLS channel) or connectivity (as in a proxy channel).
+ *
+ * Closing the [source] does not impact the [sink], and vice versa.
+ */
+interface Channel {
+  val source: Source
+  val sink: Sink
+
+  /**
+   * Fail any in-flight and future operations. After canceling:
+   *
+   *  * Any attempt to write or flush [sink] will fail immediately with an [IOException].
+   *  * Any attempt to read [source] will fail immediately with an [IOException].
+   *
+   * Closing the source and the sink will complete normally even after a channel has been canceled.
+   *
+   * This operation may be called by any thread at any time. It is safe to call concurrently while
+   * operating on the source or the sink.
+   */
+  fun cancel()
+}
