41c10fad6d7e274e9f4a079ccd3d2e5ac5e94308
Jesse Wilson <jwilson@squareup.com>
2025-07-22 16:34:13 -0400

    Visual test

--- okio/src/jvmTest/kotlin/okio/internal/MinHeapTest.kt
+++ okio/src/jvmTest/kotlin/okio/internal/MinHeapTest.kt
@@ -1,6 +1,7 @@
 package okio.internal
 
 import assertk.assertThat
+import assertk.assertions.isEqualTo
 import java.util.concurrent.TimeUnit
 import okio.AsyncTimeout
 import okio.Heap
@@ -62,6 +63,122 @@ class MinHeapTest {
       assertThat(node === heap.first())
       heap.removeFirst(node)
       validateHeap(heap)
+    }
+  }
+
+  @Test
+  fun lastElementIsGreaterThanRemovedElement() {
+    val before = """
+      |...............5................
+      |......10..............20........
+      |..11......12......21......22....
+      |""".toHeap()
+
+    before.removeFromQueue(before[10])
+
+    assertThat(before.toDebugString()).isEqualTo(
+      """
+      |...............5................
+      |......11..............20........
+      |..22......12......21............
+      |""".trimMargin()
+    )
+  }
+
+  @Test
+  fun lastElementIsLessThanRemovedElement() {
+    val before = """
+      |...............5................
+      |......20..............10........
+      |..21......22......11......12....
+      |""".toHeap()
+
+    before.removeFromQueue(before[20])
+
+    assertThat(before.toDebugString()).isEqualTo(
+      """
+      |...............5................
+      |......12..............10........
+      |..21......22......11............
+      |""".trimMargin()
+    )
+  }
+
+  @Test
+  fun lastElementIsLessThanRemovedElementParent() {
+    val before = """
+      |...............................5................................
+      |..............10..............................20................
+      |......11..............12..............21..............22........
+      |..13......14....................................................
+      |""".toHeap()
+
+    before.removeFromQueue(before[21])
+
+    assertThat(before.toDebugString()).isEqualTo(
+      """
+      |...............................5................................
+      |..............10..............................14................
+      |......11..............12..............20..............22........
+      |..13............................................................
+      |""".trimMargin()
+    )
+  }
+
+
+  private fun String.toHeap(): Heap {
+    val nodeValues = trimMargin()
+      .replace(".", " ")
+      .trim()
+      .split(Regex("\\s+", RegexOption.MULTILINE))
+      .map { it.toLong() }
+
+    val result = Heap()
+
+    val now = System.nanoTime()
+    for (i in nodeValues) {
+      val node = AsyncTimeout()
+      node.timeout(i, TIME_UNIT)
+      result.insertIntoQueue(now, node)
+    }
+
+    val formattedBack = result.toDebugString()
+    assertThat(formattedBack, "input is well-formatted").isEqualTo(this.trimMargin())
+
+    return result
+  }
+
+  private operator fun Heap.get(value: Long): AsyncTimeout {
+    return array.single { it?.timeoutNanos() == TIME_UNIT.toNanos(value) }!!
+  }
+
+  /**
+   * Returns a string like this:
+   *
+   * ```
+   * ...............5................
+   * ......12..............10........
+   * ..21......22......11............
+   * ```
+   */
+  private fun Heap.toDebugString() = buildString {
+    val nodeWidth = 4
+    val height = Integer.numberOfTrailingZeros(Integer.highestOneBit(heapSize)) + 1
+    val width = (1 shl height) - 1
+    var index = 1 // Heap's first element is at 1!
+    for (row in 0 until height) {
+      val printAt = height - row - 1
+      for (column in 1..width) {
+        if (Integer.numberOfTrailingZeros(column) != printAt || index >= array.size || array[index] == null) {
+          append(".".repeat(nodeWidth))
+        } else {
+          val nodeValue = TIME_UNIT.convert(array[index++]!!.timeoutNanos(), TimeUnit.NANOSECONDS).toString()
+          append(".".repeat(nodeWidth - nodeValue.length))
+          append(nodeValue)
+        }
+      }
+      append(".".repeat(nodeWidth))
+      append("\n")
     }
   }
 
